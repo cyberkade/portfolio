21b8edaf4c18eeb92e331eefdba727c32ff540ca
Kade Griffith <kadeg95@gmail.com>
2021-11-22 12:00:02 -0700

    set up basic routing and initialized a css reset

---
+++ src/components/Home.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Home = () => {
+  return <div>hello Home</div>;
+};
+export default Home;
---
+++ src/components/Nav.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Nav = () => {
+  return <div>hello Nav</div>;
+};
+export default Nav;
