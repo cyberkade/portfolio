adfd95ff06eb7ddf60c9e1492d5e594ce70e5fb1
Kade Griffith <kadeg95@gmail.com>
2022-03-07 14:29:48 -0700

    adds projects text

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./Styles/App.css";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
 import About from "./components/About";
+import Projects from "./components/Projects";
 import { useState } from "react";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         viewAbout={viewAbout}
       />
       <About />
+      <Projects />
     </div>
   );
 }
