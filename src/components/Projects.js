13cacc97c2844f7c46e9be230abc467883d525d7
Kade Griffith <kadeg95@gmail.com>
2022-04-07 17:39:09 -0600

    adds favicon

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -40,18 +40,8 @@ const Porfolio = ({ toggleModal }) => {
             languages="HTML / CSS"
             tag="mov"
           />
-          <Card
-            toggleModal={toggleModal}
-            name="Underdog Devs"
-            languages="React JS / Express"
-            tag="ud"
-          />
-          <Card
-            toggleModal={toggleModal}
-            name="Underdog Devs"
-            languages="React JS / Express"
-            tag="ud"
-          />
+          <Card toggleModal={toggleModal} name="" languages="" tag="" />
+          <Card toggleModal={toggleModal} name="" languages="" tag="" />
         </div>
       </section>
       <svg
